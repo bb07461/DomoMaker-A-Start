f8e6d37f15282f8a9092b0bc5d638d40fcea85e4
Ugochinyere Okogeri <98509031+bb07461@users.noreply.github.com>
2025-04-13 15:30:40 -0400

    i think its working...

---
+++ server/controllers/index.js
@@ -0,0 +1,9 @@
+//module.exports.Account = require('./Account.js');
+//module.exports.Domo = require('./Domo.js');
+const Account = require('./Account.js');
+const Domo = require('./Domo.js');
+
+module.exports = {
+  Account,
+  Domo,
+};
