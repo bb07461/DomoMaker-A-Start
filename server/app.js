e755179e1aafaee334f980f4ed756b89bca9b1fe
Ugochinyere Okogeri <98509031+bb07461@users.noreply.github.com>
2025-04-14 15:00:35 -0400

    Domo-Maker-C

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+const session = require('express-session');
+const RedisStore = require('connnect-redis').default;
+const redis = require('redis');
 
 const router = require('./router.js');
 
@@ -19,21 +22,39 @@ mongoose.connect(dbURI).catch((err) => {
     }
 });
 
-const app = express();
-
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
-
-router(app);
-
-app.listen(port, (err) => {
-    if (err) { throw err; }
-    console.log(`Listening on port ${port}`);
+const redisClient = redis.createClient({
+    url: process.env.REDISCLOUD_URL,
 });
+
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
+
+redisClient.connect().then(()=> {
+    const app = express();
+
+    app.use(helmet());
+    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+    app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+    app.use(compression());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.use(session({
+        key:'sessionid',
+        secret: 'Domo Arigato',
+        resave: false,
+        saveUninitialized: false,
+    }));
+
+    app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+    app.set('view engine', 'handlebars');
+    app.set('views', `${__dirname}/../views`);
+
+    router(app);
+
+    app.listen(port, (err) => {
+        if (err) { throw err; }
+        console.log(`Listening on port ${port}`);
+    });
+
+})
