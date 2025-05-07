ef62f6a9613da4746e6eaba4322dc32186ef4920
Ugochinyere Okogeri <98509031+bb07461@users.noreply.github.com>
2025-05-07 10:28:06 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -41,6 +41,9 @@ redisClient.connect().then(()=> {
 
     app.use(session({
         key:'sessionid',
+        store: new RedisStore({
+            client: redisClient
+        }),
         secret: 'Domo Arigato',
         resave: false,
         saveUninitialized: false,
